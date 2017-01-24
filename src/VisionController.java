20c56c5fb5d67a556d786d608150ed901c87d4dc
DriverStation <tekdemo+driverstation@github.com>
2017-01-23 20:47:41 -0800

    Start the Eclipse project for vision

---
+++ src/VisionController.java
@@ -0,0 +1,14 @@
+
+/**
+ * Vision system for the Steamworks competition
+ * 
+ * All your cameras are belong to me.
+ * 
+ * @author Skyview StormBots - FRC #2811
+ *
+ */
+public class VisionController {
+	
+	
+	
+}
