d7b053d553b6df70681b5922d0003442fced2f9b
Josh Harmon <josh@joshharmon.me>
2017-03-28 08:15:10 -0700

    Implement actual data-gathering + output at end

--- test_scripts/linreg-data-gatherer.py
+++ test_scripts/linreg-data-gatherer.py
@@ -11,6 +11,16 @@ try:
 except RuntimeError:
 	print("Error importing RPi.GPIO!  Probably need to run as root")
 
+## Dirty hack to import GRIP pipeline from parent directory
+import sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import grip
+## End dirty hack
+
 led_pin=40
 led_high = True
 GPIO.setmode(GPIO.BOARD)# Configure the pin counting system to board-style
@@ -32,6 +42,7 @@ os.system("v4l2-ctl -c backlight_compensation=0")
 # os.system("v4l2-ctl -c ")
 
 cam = cv2.VideoCapture(0)
+pipeline = grip.GripPipeline()
 
 def get_diff(mirror=False):
 	lightson_ret, lightson_img = cam.read()
@@ -57,22 +68,44 @@ def get_diff(mirror=False):
 
 	return diff_img
 
+def output_data(data):
+	for item in data:
+		print("%f  %d" % item) # Mod-item may work because we'd normally just mod a tuple with the same data
+
 if __name__ == '__main__':
 	frame = 0
-  list_of_data = []
+	list_of_data = []
 	while True:
 		diff=get_diff(mirror=True)
 		# cv2.imshow('my webcam', diff)
-		if cv2.waitKey(1) == 27: 
+		if cv2.waitKey(1) == 27:
+			temp_sort_list = []
 			file_path =  "/tmp/captured_diff%s.png"% frame
 			print("Saving last diff image to ", file_path)
 			cv2.imwrite(file_path,diff)
+			
+			pipeline.process(diff)
+			contours = pipeline.filter_contours_output
+			for contour in contours:
+				x, y, w, h = cv2.boundingRect(contour)
+				temp_sort_list.append(y)
+			
+			top_y = sys.maxsize
+			for i in range(len(temp_sort_list)):
+				if (temp_sort_list[i] < top_y): # < because top = 0, bottom = 360
+					top_y = temp_sort_list[i]
+			# Now we have our topmost contour's top y-coordinate
+			# Pair that up with a distance and profit
+			distance = float(raw_input("Distance of capture: "))
+			list_of_data.append((distance, top_y))
+			
 			frame +=1
 			#break  # esc or enter to quit
 	
-	file_path =  "/tmp/captured_diff.png"
+	file_path =  "/tmp/captured_diff%s.png" % frame
 	print("Saving last diff image to ", file_path)
 	cv2.imwrite(file_path,diff)
+	output_data(list_of_data)
 	print("Cleaning up!!")
 	cv2.destroyAllWindows()
 	GPIO.cleanup(led_pin)
